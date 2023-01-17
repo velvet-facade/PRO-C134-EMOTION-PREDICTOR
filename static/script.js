cf882127c6e5ea0469a9203fc9b828987c44c033
ANANYA KANNAN <104369167+velvet-facade@users.noreply.github.com>
2023-01-17 22:47:30 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+    console.log('Ready')
+
+    //  Fetch the current date and update it in the DOM
+    date = new Date()
+    currentDate = date.toLocaleDateString()
+
+
+
+    //  write an event, when Submit button is clicked
+    $('#button').click(function(){
+
+        //  get the text value from the textarea using the 'val()' method
+        let text_value = $('#text').val()
+
+        //  Convert it to JS object.
+        //  Provide a 'key' here and in write the same in app.py file as well to extract data
+        let input_text = {'key' : text_value}
+        console.log(input_text)
+
+        //  ajax request
+        $.ajax({
+
+            //  type of web request
+            type : 'POST',
+
+            //  Data to be sent in JSON format
+            data : JSON.stringify(),
+
+            //  type of response expected is json
+            dataType : 'json',
+
+            //  contentType
+            contentType : 'application/json',
+
+            //  if everything is successful, run this function
+            success : function(result){
+
+                // extract prediction and emoticon url from result
+                let prediction = result.prediction
+                let emoticon_url = result.result
+
+                console.log(emoticon_url)
+                
+                $('#sentiment').text(prediction)
+                $('#sentiment').show()
+
+                $('#emoji').attr('src' , emoticon_url)
+                $('#emoji').show()
+
+            },
+
+            //  if any error, run this function
+            error : function(result){
+
+                console.log(result)
+            }
+        })
+
+
+        //  clearing the textbox after every button push
+        $('#text').val("")
+    })
+        
+})
